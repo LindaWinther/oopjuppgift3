ce8058268c74292a653e9eddf8a49a5270a6bc5f
Linda Winther <Linda.Winther@outlook.com>
2025-11-03 14:06:04 +0100

    Börjar med knapparna

--- src/Buttons.java
+++ src/Buttons.java
@@ -1,6 +1,23 @@
+import javax.swing.*;
+
 public class Buttons {
 
 //  Linda
 
+    JButton one = new JButton("1");
+    JButton two = new JButton("2");
+    JButton three = new JButton("3");
+    JButton four = new JButton("4");
+    JButton five = new JButton("5");
+    JButton six = new JButton("6");
+    JButton seven = new JButton("7");
+    JButton eight = new JButton("8");
+    JButton nine = new JButton("9");
+    JButton ten = new JButton("10");
+    JButton eleven = new JButton("11");
+    JButton twelve = new JButton("12");
+    JButton thirteen = new JButton("13");
+    JButton fourteen = new JButton("14");
+    JButton fiveteen = new JButton("15");
 
 }
