158c68294d59665b63537e813da19bed7de604d1
Linda Winther <Linda.Winther@outlook.com>
2025-11-04 13:33:14 +0100

    Nu rör sig knapparna

--- src/Board.java
+++ src/Board.java
@@ -7,9 +7,12 @@ public class Board extends JFrame {
     private int gridSize = 4;
     //private Button[][] buttons;
     public Button[][] buttons = new Button[gridSize][gridSize]; // Här
+    private final int [][] tiles = new int [gridSize][gridSize]; // ta bort ovan?
+    // Vart nollan är
+    int zeroWidth = 3;
+    int zeroHight = 3;
 
     public Board() {
-        setVisible(true);
         setTitle("15 puzzle");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -19,13 +22,12 @@ public class Board extends JFrame {
         int number = 1;
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
-                if (number < gridSize * gridSize) {
-                    buttons[i][j] = new Button(number++);
-                } else {
-                    buttons[i][j] = new Button(0);
-                }
-                int w = i;
-                int h = j;
+                tiles[i][j] = (number < gridSize * gridSize) ? number++ : 0;
+                buttons[i][j] = new Button(tiles[i][j]);
+                final int w = i, h = j;
+
+              //  int w = i;
+               // int h = j;
 
                 buttons[i][j].addActionListener(new ActionListener() {
                     @Override
@@ -36,34 +38,47 @@ public class Board extends JFrame {
                 add(buttons[i][j]);
             }
         }
+        zeroWidth = gridSize -1;
+        zeroHight = gridSize -1;
+        updateBoard();
+        setVisible(true); // flyttad sist så allt kommer med
     }
 
     public void isSolved (){
     }
 
 
-    // Vart nollan är
-    int zeroWith = 3;
-    int zeroHight = 3;
-    public void moveButton (int with, int hight) {
+
+    public void moveButton (int width, int hight) {
 
         boolean adjacent =
-                (with - zeroWith == 1) && (hight == zeroHight) ||
-                (hight - zeroHight == 1) && (with == zeroWith);
+                (Math.abs (width - zeroWidth) == 1 && (hight == zeroHight)) ||
+                (Math.abs(hight - zeroHight) == 1 && (width == zeroWidth));
 
+        if (!adjacent) return;
+
+        int tempoary = tiles[width][hight];
+        tiles[width][hight] = tiles [zeroWidth][zeroHight];
+        tiles[zeroWidth][zeroHight] = tempoary;
+
+        zeroWidth = width;
+        zeroHight = hight;
+
+        updateBoard();
+        /*
         if (adjacent) {
             String saveNumber = buttons[with][hight].getText();
-            buttons[with][hight].setText("");
-            buttons[zeroWith][zeroHight].setText(saveNumber);
+            buttons[with][hight].setText("");  // byt knapp istället för namn
+            buttons[zeroWidth][zeroHight].setText(saveNumber);
 
-            zeroWith = with;
+            zeroWidth = with;
             zeroHight = hight;
 
             updateBoard();
-        }
+        }*/
     }
 
-    int [][] tiles = new int [gridSize][gridSize];
+   // int [][] tiles = new int [gridSize][gridSize];
     public void updateBoard() {
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
