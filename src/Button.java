f37e2e558834d86bf5bfe9f0198829d2c018a886
Linda Winther <Linda.Winther@outlook.com>
2025-11-04 21:51:41 +0100

    inte klar

--- src/Button.java
+++ src/Button.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Button extends JButton {
     private int number;
@@ -18,5 +20,24 @@ public class Button extends JButton {
             setText("");
         } else
             setText(String.valueOf(number));
+    }
+
+    public void newGame() { // Det här känns inte rätt men jag återkommer imorgon,
+        // jag komplicerade jättemycket och kom fram till att vi redan hade dom två metoderna som fanns.
+      /*  newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RandomiseBoard();
+                updateBoard();
+            }
+
+            private void updateBoard() {
+            }
+
+            private void RandomiseBoard() {
+            }
+        });
+
+       */
     }
 }
